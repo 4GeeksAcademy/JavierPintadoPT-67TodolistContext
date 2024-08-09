592c4fcdb60203d989a7c868dee23d0e57fac919
javier5pe <72091566+javier5pe@users.noreply.github.com>
2024-08-09 09:25:12 +0000

    e

--- src/js/component/Card.jsx
+++ src/js/component/Card.jsx
@@ -19,32 +19,44 @@ const Card = ({name, email, phone, address, id}) => {
 
     console.log(store.contactoSeleccionado)
     return (
-        <div className=" carta mt-5"style={{width: "28rem"}}>
-            
-            <img src="https://cdn.pixabay.com/photo/2016/11/23/00/57/adult-1851571_1280.jpg" className="card-img-top imagen" alt="..."/>
+        <div className=" border  w-75 carta mt-5"style={{width: "28rem"}}>
+                                    
+
+            <img src="https://cdn.pixabay.com/photo/2016/11/23/00/57/adult-1851571_1280.jpg" className=" ms-5 card-img-top imagen" alt="..."/>
             
                 <div className="card-body">
-                    <div container>
-                        <div className="row">
-                            <h5 className="  ms-5 col-2 nombre card-title">{name}</h5>
+                    <div className="row">
+                        <h5 className="col-6 nombre">{name}</h5>
+                        <p className="col-4"></p>
                         
-                            <i onClick={() => handleEditar(name, email, phone, address, id) }
-                            className=" col-2 lapiz fas fa-pencil-alt">
-                            </i>
+                        <i onClick={() => handleEditar(name, email, phone, address, id) }
+                            className=" fas fa-pencil-alt col-1">
+                        </i>
                             
-                            <i onClick={() => actions.eliminarContacto(id) }
-                            className=" col-2 basura fas fa-trash-alt">
-                            </i>
-                        </div>
+                         <i onClick={() => actions.eliminarContacto(id) }
+                            className="  fas fa-trash-alt col-1 ">
+                        </i>
                     </div>
-                   
+                    <div className="row">
+                        <p className="col-6 datos">
+                            <i className="fas fa-envelope me-2"></i>{email}
+                        </p>
+                        
+                        <p className=" col-6"></p>
+                        
+                        <p className="col-6  datos">
+                            <i className="fas fa-phone me-2 "></i>{phone}
+                        </p>
+                        
+                        <p className=" col-6"></p>
+                        
+                        <p className=" col-6  datos"> 
+                            <i className="fas fa-home me-2 "></i>{address}
+                        </p>
+                        
+                        <p className=" col-6"></p>
+                    </div>
                     
-                    <p className=" datos card-text">{email}</p>
-                    <p className="datos card-text">{phone}</p>
-                    <p className=" datos card-text">{address}</p>
-                    
-                   
-                   
                 </div>
         </div>
     )
